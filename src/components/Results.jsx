6f15b321af24b4d3487bbcf72c857f42855ee040
hugomarti <hgmarti@gmail.com>
2020-09-19 12:09:04 +0200

    minor code cleaning

---
+++ src/components/Results.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Result from "./Result";
+
+function Results({ results, openPopup }) {
+  return (
+    <section className="results">
+      {results.map((result) => (
+        <Result key={result.imbdID} result={result} openPopup={openPopup} />
+      ))}
+    </section>
+  );
+}
+
+export default Results;
